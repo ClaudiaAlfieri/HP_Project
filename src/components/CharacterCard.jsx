f4016c1db8f20c88f5a57811311fb1ed3389bf93
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-07-27 13:53:07 -0300

    Update CharacterCard.tsx to change colors

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,7 +1,24 @@
 function CharacterCard({ character, onRemove }) {
+  function getHouseClass(house) {
+    switch (house) {
+      case "Gryffindor":
+        return "card gryffindor";
+      case "Slytherin":
+        return "card slytherin";
+      case "Hufflepuff":
+        return "card hufflepuff";
+      case "Ravenclaw":
+        return "card ravenclaw";
+      default:
+        return "card sem-casa";
+    }
+  }
+
   return (
-    <div className="card">
-      {character.image && <img src={character.image} alt={character.name} width={80} />}
+    <div className={getHouseClass(character.house)}>
+      {character.image && (
+        <img src={character.image} alt={character.name} width={80} />
+      )}
       <h3>{character.name}</h3>
       <p>Casa: {character.house || "desconhecida"}</p>
       <p>Género: {character.gender || "desconhecido"}</p>
